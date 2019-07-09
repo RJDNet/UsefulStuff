1898dc9b3ec982ddea70a60edc8b3b86282ea925
RJDNet <russdando86@gmail.com>
2019-07-09 07:02:48 +0000

    Added StopWatch

---
+++ stopwatch/src/App.tsx
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import * as Redux from 'redux';
+import * as ReactRedux from 'react-redux';
+
+import './App.css';
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION__: Function
+  }
+}
+
+interface IStopWatchProps {
+  time: number
+  running: Boolean
+  onStop: () => void
+  onStart: () => void
+}
+
+interface IStopWatchState {
+  running: Boolean
+  time: number
+}
+
+let reducer = (state = { running: false, time: 0 }, action: { type: string }) => {
+  switch (action.type) {
+    case 'START': return Object.assign({}, state, { running: true });
+    case 'STOP': return Object.assign({}, state, { running: false });
+    case 'TICK': return Object.assign({}, state, { time: state.time + (state.running ? 1 : 0) });
+    default: return state;
+  }
+};
+
+let container = Redux.createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+const mapStateToProps = (state: IStopWatchState) => state;
+
+const mapDispatchToProps = (dispatch: Function, props: IStopWatchProps) => ({
+  onStart: () => { dispatch({ type: 'START' }); },
+  onStop: () => { dispatch({ type: 'STOP' }); }
+});
+
+let StopWatch = ReactRedux.connect(mapStateToProps, mapDispatchToProps)((props: IStopWatchProps) => {
+  let { time, running, onStop, onStart } = props;
+
+  let minutes = Math.floor(time / 60);
+  let seconds = time - (minutes * 60);
+  let secondsFormatted = `${seconds < 10 ? '0' : ''}${seconds}`;
+
+  return <div>
+    <p>{minutes}:{secondsFormatted}</p>
+    <button onClick={running ? onStop : onStart}>{running ? 'Stop' : 'Start'}</button>
+  </div>
+});
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ReactRedux.Provider store={container}>
+          <StopWatch />
+        </ReactRedux.Provider>
+      </div>
+    );
+  }
+}
+
+setInterval(() => {
+  container.dispatch({ type: 'TICK' });
+}, 1000);
+
+export default App;
